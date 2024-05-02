10cc8e133468c15c89847d98de7afee430d531b4
1rubiz <1izekorruby@gmail.com>
2024-05-02 17:11:00 +0100

    quick fix

--- src/components/dateFormat.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "../dateFormat" {
-    export function dateFormat(date: Date): string;
-}
--- src/components/utils/store.ts
+++ src/components/utils/store.ts
@@ -1,32 +1,66 @@
-// store.js
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
 interface DataStoreState {
   blog: any;
-  moreBlog: any;
+  moreBlog: any[]; // Change this to array type
   loading: boolean;
 }
 
 export const dataStore = defineStore('user', {
-  id: 'myStore',
   state: (): DataStoreState => ({
     blog: {},
     moreBlog: [],
     loading: false
   }),
   actions: {
-    async getBlogData(id) {
+    async getBlogData(id: string) {
       try {
         this.loading = true;
-        const response = await axios.get('https://techcrunch.com/wp-json/wp/v2/posts/'+id+'?per_page=1');
-        const moreBlogResponse = await axios.get('https://techcrunch.com/wp-json/wp/v2/posts?per_page=10&per_page=3')
+        const response = await axios.get('https://techcrunch.com/wp-json/wp/v2/posts/' + id + '?per_page=1');
+        const moreBlogResponse = await axios.get('https://techcrunch.com/wp-json/wp/v2/posts?per_page=10&per_page=3');
         this.moreBlog = moreBlogResponse.data;
         this.blog = response.data;
-        this.loading = false;
       } catch (error) {
         console.error('Error fetching user data:', error);
+      } finally {
+        this.loading = false;
       }
     }
   }
 });
+
+
+
+
+// // store.js
+// import { defineStore } from 'pinia';
+// import axios from 'axios';
+
+// interface DataStoreState {
+//   blog: any;
+//   moreBlog: any;
+//   loading: boolean;
+// }
+
+// export const dataStore = defineStore('user', {
+//   state: (): DataStoreState => ({
+//     blog: {},
+//     moreBlog: [],
+//     loading: false
+//   }),
+//   actions: {
+//     async getBlogData(id: string) {
+//       try {
+//         this.loading = true;
+//         const response = await axios.get('https://techcrunch.com/wp-json/wp/v2/posts/'+id+'?per_page=1');
+//         const moreBlogResponse = await axios.get('https://techcrunch.com/wp-json/wp/v2/posts?per_page=10&per_page=3')
+//         this.moreBlog = moreBlogResponse.data;
+//         this.blog = response.data;
+//         this.loading = false;
+//       } catch (error) {
+//         console.error('Error fetching user data:', error);
+//       }
+//     }
+//   }
+// });
